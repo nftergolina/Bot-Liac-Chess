d49d1235f8ef6bff6e55d4cd20e28e73b2b660b9
Natália Felix Tergolina <natiftg1@hotmail.com>
2015-01-07 04:09:23 -0200

    Clearer Code

--- Bot_Natalia.py
+++ Bot_Natalia.py
@@ -39,17 +39,17 @@ class MyBot(LiacBot):
         del moveList[:]
         
         i = 0
-        while move[0] < 2**63:
-            move[0] = move[0] << 1
+        while move[0] > 0:
+            move[0] = move[0] >> 1
             i += 1
-        ArgFrom = (i//8, i%8)
+        ArgFrom = (i//8, 7-(i%8))
         print 'From: ', ArgFrom
            
         i = 0
-        while move[1] < 2**63:
-            move[1] = move[1] << 1
+        while move[1] > 0:
+            move[1] = move[1] >> 1
             i += 1
-        ArgTo = (i//8, i%8)
+        ArgTo = (i//8, 7-(i%8))
         print 'To:   ', ArgTo
     
         print 'Move translated'
@@ -68,66 +68,34 @@ class MyBot(LiacBot):
 class Board(object):
     def __init__(self, state):
         self.AllPieces = []
-        self.Pawns = []
-        self.Rooks = []
-        self.Bishops = []
-        self.Queens = []
-        self.Knights = []
         self.OccupiedCells = {}
-        self.EmptyCells  = 0
-        self.Value = 0
         self.BotTeam = None
 
         self.getBotTeam(state)
-    
-    
-
         self.getIndividualPositions(state)
-            
-        self.AllPieces.extend(self.Pawns)
-        self.AllPieces.extend(self.Rooks)
-        self.AllPieces.extend(self.Bishops)
-        self.AllPieces.extend(self.Queens)
-        self.AllPieces.extend(self.Knights)
 
         self.OccupiedCells['white'] = 0
         self.OccupiedCells['black'] = 0
         
         for p in self.AllPieces:
-            if p.team == 'white':
-                self.OccupiedCells['white'] += p.position
-            elif p.team == 'black':
-                self.OccupiedCells['black'] += p.position
-
+            self.OccupiedCells[p.team] += p.position
+        
         self.EmptyCells = ~( self.OccupiedCells['white'] | self.OccupiedCells['black'] )
 
         for p in self.AllPieces:
             p.generatePossibleMoves(self)
 
-        self.getBoardValue()
 
 
     def getBotTeam(self, state):
         if state['who_moves'] == WHITE:
             self.BotTeam = 'white'
-        elif state['who_moves'] == BLACK:
+        else:
             self.BotTeam = 'black'
             
 
-    def getBoardValue(self):
-        for p in self.AllPieces:
-            if p.team == self.BotTeam:
-                self.Value += p.value
-            else:
-                self.Value -= p.value
                 
     def getIndividualPositions(self, state):
-        i = 0
-        j = 0
-        k = 0
-        l = 0
-        m = 0
-        n = 0
         bitboardBase = 0b1000000000000000000000000000000000000000000000000000000000000000
         for p in state['board']:
             if p == p.lower():
@@ -135,27 +103,16 @@ class Board(object):
             else:
                 team = 'white'
             if p.lower() == 'p':
-                self.Pawns.append(Pawn(bitboardBase >> i, team, self))
-                j += 1
+                self.AllPieces.append(Pawn(bitboardBase, team, self))
             elif p.lower() == 'r':
-                self.Rooks.append(Rook(bitboardBase >> i, team, self))
-                k += 1
+                self.AllPieces.append(Rook(bitboardBase, team, self))     
             elif p.lower() == 'b':
-                self.Bishops.append(Bishop(bitboardBase >> i, team, self))
-                l += 1
+                self.AllPieces.append(Bishop(bitboardBase, team, self))                
             elif p.lower() == 'q':
-                self.Queens.append(Queen(bitboardBase >> i, team, self))
-                m += 1
+                self.AllPieces.append(Queen(bitboardBase, team, self))                
             elif p.lower() == 'n':
-                self.Knights.append(Knight(bitboardBase >> i, team, self))
-                n += 1
-            i += 1
-            
-        Pawn.Alive = j
-        Rook.Alive = k
-        Bishop.Alive = l
-        Queen.Alive = m
-        Knight.Alive = n
+                self.AllPieces.append(Knight(bitboardBase, team, self))              
+            bitboardBase = bitboardBase >> 1
 
         
 
@@ -165,31 +122,13 @@ class Piece(object):
         self.position = None
         self.team = None
         self.possibleMoves = []
-        self.value = None
-        self.char = None
         
 
 class Pawn(Piece):
-    Alive = None
     def __init__(self, p, t, board):
         self.position = p
         self.team = t
-        if self.team == 'black':
-            self.char = 'p'
-            i = 0
-            aux = self.position
-            while aux < 2**64:
-                aux = aux << 8
-                i += 1
-            self.value = i*i
-        elif self.team == 'white':
-            self.char = 'P'
-            i = 0
-            aux = self.position
-            while aux > 0:
-                aux = aux >> 8
-                i += 1
-            self.value = i*i      
+        
         
     def generatePossibleMoves(self, board):
         SecondRow  = 0b0000000010000000000000000000000000000000000000000000000000000000
@@ -200,28 +139,22 @@ class Pawn(Piece):
 
         moves = []
 
-        '''if self.team == 'black' and ((self.position >> 8) & board.OccupiedCells['white']) == 0:
+        if self.team == 'black' and ((self.position >> 8) & board.OccupiedCells['white']) == 0:
             moves.append(self.position >> 8)
             if (self.position >= SecondRow) and (self.position > ThirdRow) and ((self.position >> 16) & board.OccupiedCells['white']) == 0:
                 moves.append(self.position >> 16)
         elif self.team == 'white' and ((self.position << 8) & board.OccupiedCells['black']) == 0:
             moves.append(self.position << 8)
             if (self.position >= SeventhRow) and (self.position < EighthRow) and ((self.position << 16) & board.OccupiedCells['black']) == 0:
-                moves.append(self.position << 16)'''
+                moves.append(self.position << 16)
                 
         self.possibleMoves = moves
    
 
 class Rook(Piece):
-    Alive = None
     def __init__(self, p, t, board):
         self.position = p
         self.team = t
-        if self.team == 'black':
-            self.char = 'r'
-        elif self.team == 'white':
-            self.char = 'R'
-        self.value = 10
         
 
     def generatePossibleMoves(self, board):
@@ -253,15 +186,9 @@ class Rook(Piece):
 
 
 class Bishop(Piece):
-    Alive = None
     def __init__(self, p, t, board):
         self.position = p
         self.team = t
-        if self.team == 'black':
-            self.char = 'b'
-        elif self.team == 'white':
-            self.char = 'B'
-        self.value = 12
         
 
     def generatePossibleMoves(self, board):
@@ -293,15 +220,9 @@ class Bishop(Piece):
         
 
 class Queen(Piece):
-    Alive = None
     def __init__(self, p, t, board):
         self.position = p
         self.team = t
-        if self.team == 'black':
-            self.char = 'q'
-        elif self.team == 'white':
-            self.char = 'Q'
-        self.value = 20
         
 
     def generatePossibleMoves(self, board):
@@ -352,19 +273,12 @@ class Queen(Piece):
         self.possibleMoves = moves
 
 class Knight(Piece):
-    Alive = None
     def __init__(self, p, t, board):
         self.position = p
         self.team = t
-        if self.team == 'black':
-            self.char = 'n'
-        elif self.team == 'white':
-            self.char = 'N'
-        self.value = 10
         
-
     def generatePossibleMoves(self, board):
-        '''FirstColumn = 0b1000000010000000100000001000000010000000100000001000000010000000
+        FirstColumn = 0b1000000010000000100000001000000010000000100000001000000010000000
         SecondColumn = 0b0100000001000000010000000100000001000000010000000100000001000000
         SeventhColumn = 0b0000001000000010000000100000001000000010000000100000001000000010
         EighthColumn = 0b0000000100000001000000010000000100000001000000010000000100000001
@@ -396,7 +310,7 @@ class Knight(Piece):
                 
         for m in moves:
             if (m < 1) or (m > 2**63) or ((m & board.OccupiedCells[self.team]) != 0):
-                moves.remove(m)'''
+                moves.remove(m)
         moves = []
 
         self.possibleMoves = moves
